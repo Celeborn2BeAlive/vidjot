4316431391deb34e184a7a28dd9463eb7f912841
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2018-06-13 14:16:30 +0200

    Add views and connection to mongo db

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 import express from 'express'
+import exphbs from 'express-handlebars'
+import mongoose from 'mongoose'
 
 const app = express()
 
+// Connect to mongoose
+async function connectToDatabase() {
+    try {
+        await mongoose.connect('mongodb://localhost/vidjot-dev');
+        console.log('MongoDB connected...')
+    } catch(err) {
+        console.error(err)
+    }
+}
+connectToDatabase()
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+
 // Index route
 app.get('/', (req, res) => {
-    res.send('INDEX')
+	const title = 'Welcome !'
+	res.render('index', {
+		title: title
+	})
 })
 
 // About route
 app.get('/about', (req, res) => {
-    res.send('ABOUT')
+	res.render('about')
 })
 
+// Run server
 const port = 5000;
 app.listen(port, () =>  {
-    console.log(`Server started on port ${port}`)
+	console.log(`Server started on port ${port}`)
 })
