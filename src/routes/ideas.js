ae17ec53434b3ea17d7434d3c6612948a134e4d4
Laurent NOEL <laurent.noel.c2ba@gmail.com>
2018-07-12 13:58:50 +0200

    34. Access control

--- src/routes/ideas.js
+++ src/routes/ideas.js
@@ -7,7 +7,9 @@ const router = express.Router()
 
 // Idea List
 router.get('/', ensureAuthenticated, async (req, res) => {
-    const ideas = await Idea.find({}).sort({ date: 'descending' })
+    const ideas = await Idea.find({
+        user: req.user.id
+    }).sort({ date: 'descending' })
     res.render('ideas/index', { ideas })
 })
 
@@ -36,34 +38,51 @@ router.post('/', ensureAuthenticated, async (req, res) => {
         return
     }
 
-    const newUser = { ...req.body }
+    const newUser = { ...req.body, user: req.user.id }
     await new Idea(newUser).save()
 
     req.flash('success_msg', 'Video idea added')
     res.redirect('/ideas')
 })
 
+function ensureUser(idea, req, res, next) {
+    if (idea.user == req.user.id) {
+        return next();
+    }
+    req.flash('error_msg', 'Not authorized')
+    res.redirect('/ideas')
+}
+
 // Edit Idea Form
 router.get('/edit/:id', ensureAuthenticated, async (req, res) => {
     const idea = await Idea.findOne({
         _id: req.params.id
     })
-    res.render('ideas/edit', { idea })
+    ensureUser(idea, req, res, () => {
+        res.render('ideas/edit', { idea })
+    })
 })
 
 // Edit Idea form process
 router.put('/:id', ensureAuthenticated, async (req, res) => {
     const idea = await Idea.findOne({ _id: req.params.id })
-    idea.title = req.body.title
-    idea.details = req.body.details
-    await idea.save()
-    req.flash('success_msg', 'Video idea updated')
-    res.redirect('/ideas')
+    ensureUser(idea, req, res, async () => {
+        idea.title = req.body.title
+        idea.details = req.body.details
+        await idea.save()
+        req.flash('success_msg', 'Video idea updated')
+        res.redirect('/ideas')
+    })
 })
 
 // Delete Idea
 router.delete('/:id', ensureAuthenticated, async (req, res) => {
-    await Idea.remove({ _id: req.params.id })
+    const result = await Idea.deleteOne({ _id: req.params.id, user: req.user.id })
+    if (result.n == 0) {
+        req.flash('error_msg', 'Not authorized')
+        res.redirect('/ideas')
+        return
+    }
     req.flash('success_msg', 'Video idea removed')
     res.redirect('/ideas')
 })
